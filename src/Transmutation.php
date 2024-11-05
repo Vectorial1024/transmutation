9194a8cce068c818e2c8e0a807cfe795dc63f802
Vincent Wong <vincentwwy168@gmail.com>
2024-11-05 16:25:53 +0800

    Rely on alof-lib to implement keys() and values()

--- src/Transmutation.php
+++ src/Transmutation.php
@@ -5,22 +5,23 @@ namespace Vectorial1024\Transmutation;
 use ArrayAccess;
 use ArrayObject;
 use Traversable;
+use Vectorial1024\AlofLib\Alof;
 
 /**
- * The "collections" class for array-like objects. Write clean and tested code for iterating over array-like objects.
- * @template TKey
+ * The "collections" class for array-like objects.
+ * @template T of ArrayAccess&Traversable
  */
 class Transmutation
 {
     /**
      * The array-like object instance to transmute on.
-     * @var ArrayAccess<TKey, mixed>&Traversable<Tkey, mixed>
+     * @var ArrayAccess&Traversable
      */
     private ArrayAccess&Traversable $alo;
 
     /**
      * Creates an instance of Transmutation.
-     * @param ArrayAccess<TKey, mixed>&Traversable<TKey, mixed> $alo The array-like object instance to transmute on.
+     * @param ArrayAccess&Traversable $alo The array-like object instance to transmute on.
      */
     public function __construct(ArrayAccess&Traversable $alo)
     {
@@ -28,44 +29,24 @@ class Transmutation
     }
 
     /**
-     * Returns the keys of the given array-like object, with optional filtering.
+     * Returns the keys of this transmutation, with optional filtering.
      * @param mixed $filter_value (optional) the value to filter by
      * @param bool $strict (optional) whether to use strict comparison (===) while filtering
-     * @return static
-     * @see array_keys() for equivalent behavior in arrays
+     * @return static<ArrayObject>
      */
     public function keys(mixed $filter_value = null, bool $strict = false): static
     {
-        // use separate loops for best performance
-        $result = [];
-        if (!isset($filter_value)) {
-            // keys only
-            foreach ($this->alo as $key => $value) {
-                $result[] = $key;
-            }
-            return new static(new ArrayObject($result));
-        }
-        if (!$strict) {
-            // non-strict filtering
-            foreach ($this->alo as $key => $value) {
-                if ($key != $filter_value) {
-                    continue;
-                }
-                $result[] = $key;
-            }
-            return new static(new ArrayObject($result));
-        }
-        // strict filtering
-        if ($this->alo instanceof WeakMap || $this->alo instanceof SplFixedArray || $this->alo instanceof SplDoublyLinkedList) {
-            // these types have strict key types; either the key exists, or it does not.
-            return isset($this->alo[$filter_value]) ? new static(new ArrayObject($filter_value)) : new static(new ArrayObject());
-        }
-        foreach ($this->alo as $key => $value) {
-            if ($key !== $filter_value) {
-                continue;
-            }
-            $result[] = $key;
-        }
+        $result = Alof::alo_keys($this->alo, $filter_value, strict: $strict);
+        return new static(new ArrayObject($result));
+    }
+
+    /**
+     * Returns the values of this transmutation.
+     * @return static<ArrayObject>
+     */
+    public function values(): static
+    {
+        $result = Alof::alo_values($this->alo);
         return new static(new ArrayObject($result));
     }
 }
