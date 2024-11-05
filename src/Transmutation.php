16d65fc729aac6192a37c59e0e23fe73d44511f5
Vincent Wong <vincentwwy168@gmail.com>
2024-11-05 12:08:24 +0800

    Setup PHPUnit

---
+++ src/Transmutation.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Vectorial1024\Transmutation;
+
+/**
+ * The "collections" class for array-like objects. Write clean and tested code for iterating over array-like objects.
+ */
+class Transmutation
+{
+    private ArrayAccess&Traversable $aloInstance;
+
+    public function __construct()
+    {
+
+    }
+}
---
+++ tests/TransmutationTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Vectorial1024\Transmutation\Test;
+
+use PHPUnit\Framework\TestCase;
+
+class TransmutationTest extends TestCase
+{
+    public function testNothing()
+    {
+        $this->assertTrue(true);
+    }
+}
